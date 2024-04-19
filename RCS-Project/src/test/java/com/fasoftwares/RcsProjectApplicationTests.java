417a6435e4997c2180693868c1ef9d19dd32978f
user <shivakumar.github@gmail.com>
2024-04-19 18:43:36 +0530

    RCS project creation

---
+++ RCS-Project/src/test/java/com/fasoftwares/RcsProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fasoftwares;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RcsProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
